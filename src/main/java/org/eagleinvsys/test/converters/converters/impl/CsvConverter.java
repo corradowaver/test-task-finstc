076526673422cc87a529720c513e4641843a551d
corradowaver <corradowave1@gmail.com>
2022-03-28 15:46:27 +0300

    converters impl

--- src/main/java/org/eagleinvsys/test/converters/converters/impl/CsvConverter.java
+++ src/main/java/org/eagleinvsys/test/converters/converters/impl/CsvConverter.java
@@ -2,20 +2,50 @@ package org.eagleinvsys.test.converters.converters.impl;
 
 import org.eagleinvsys.test.converters.converters.Converter;
 import org.eagleinvsys.test.converters.convertibles.ConvertibleCollection;
+import org.eagleinvsys.test.converters.convertibles.ConvertibleMessage;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class CsvConverter implements Converter {
 
-    /**
-     * Converts given {@link ConvertibleCollection} to CSV and outputs result as a text to the provided {@link OutputStream}
-     *
-     * @param collectionToConvert collection to convert to CSV format
-     * @param outputStream        output stream to write CSV conversion result as text to
-     */
-    @Override
-    public void convert(ConvertibleCollection collectionToConvert, OutputStream outputStream) {
-        // TODO: implement
+  /**
+   * Converts given {@link ConvertibleCollection} to CSV and outputs result as a text to the provided {@link OutputStream}
+   *
+   * @param collectionToConvert collection to convert to CSV format
+   * @param outputStream        output stream to write CSV conversion result as text to
+   */
+  @Override
+  public void convert(ConvertibleCollection collectionToConvert, OutputStream outputStream) {
+    byte[] output = makeCsv(collectionToConvert).getBytes(UTF_8);
+    try {
+      outputStream.write(output);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
+
+  public String makeCsv(ConvertibleCollection collectionToConvert) {
+    Set<String> headers = (Set<String>) collectionToConvert.getHeaders();
+    List<ConvertibleMessage> records = (List<ConvertibleMessage>) collectionToConvert.getRecords();
+
+    StringBuilder builder = new StringBuilder();
+
+    builder.append(String.join(";", headers));
+    builder.append("\n");
+    records.forEach(message -> {
+          List<String> row = headers.stream().map(message::getElement).collect(Collectors.toList());
+          builder.append(String.join(";", row));
+          builder.append("\n");
+        }
+    );
+    return builder.toString();
+  }
+
 
 }
--- src/main/java/org/eagleinvsys/test/converters/converters/impl/StandardCsvConverter.java
+++ src/main/java/org/eagleinvsys/test/converters/converters/impl/StandardCsvConverter.java
@@ -1,28 +1,38 @@
 package org.eagleinvsys.test.converters.converters.impl;
 
 import org.eagleinvsys.test.converters.converters.StandardConverter;
+import org.eagleinvsys.test.converters.convertibles.ConvertibleCollection;
+import org.eagleinvsys.test.converters.convertibles.impl.Message;
+import org.eagleinvsys.test.converters.convertibles.impl.MessageCollection;
 
 import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StandardCsvConverter implements StandardConverter {
 
-    private final CsvConverter csvConverter;
+  private final CsvConverter csvConverter;
 
-    public StandardCsvConverter(CsvConverter csvConverter) {
-        this.csvConverter = csvConverter;
-    }
+  public StandardCsvConverter(CsvConverter csvConverter) {
+    this.csvConverter = csvConverter;
+  }
 
-    /**
-     * Converts given {@link List<Map>} to CSV and outputs result as a text to the provided {@link OutputStream}
-     *
-     * @param collectionToConvert collection to convert to CSV format. All maps must have the same set of keys
-     * @param outputStream        output stream to write CSV conversion result as text to
-     */
-    @Override
-    public void convert(List<Map<String, String>> collectionToConvert, OutputStream outputStream) {
-        // TODO: implement by using csvConverter
-    }
+  /**
+   * Converts given {@link List<Map>} to CSV and outputs result as a text to the provided {@link OutputStream}
+   *
+   * @param collectionToConvert collection to convert to CSV format. All maps must have the same set of keys
+   * @param outputStream        output stream to write CSV conversion result as text to
+   */
+  @Override
+  public void convert(List<Map<String, String>> collectionToConvert, OutputStream outputStream) {
+    ConvertibleCollection collection =
+        new MessageCollection(
+            collectionToConvert.stream()
+                .map(Message::new)
+                .collect(Collectors.toList())
+        );
+    csvConverter.convert(collection, outputStream);
+  }
 
 }
